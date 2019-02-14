ca9c5711c6ea7fab8ac1102892d75a86cce93fe7
Joaquín Leonel Robles <joaquin@reweb.com.ar>
2019-02-14 13:07:31 -0300

    Base de proyecto Spring Boot con MariaDB

---
+++ src/main/java/com/mil/deliveryfast/DeliveryfastApplication.java
@@ -0,0 +1,14 @@
+package com.mil.deliveryfast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryfastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryfastApplication.class, args);
+	}
+
+}
+
