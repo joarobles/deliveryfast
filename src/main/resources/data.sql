3938e1082559822756534d382883fbe06d1cb292
Joaquín Leonel Robles <joaquin@reweb.com.ar>
2019-02-14 13:33:25 -0300

    Archivo SQL con datos iniciales

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO `deliveryfast`.`ciudad` (`nombre`) VALUES ('Córdoba');
+INSERT INTO `deliveryfast`.`forma_de_pago` (`nombre`) VALUES ('Efectivo');
+INSERT INTO `deliveryfast`.`usuario` (`nombre`, `ciudad_id`) VALUES ('Pablo Auster', '1');
